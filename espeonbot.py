4c06bd28fe908c78036b59d4e3ab19a14a50fd6d
Madelaine Dalangin <madelainedalangin@gmail.com>
2026-04-23 01:56:45 -0600

    added help cmd

--- espeonbot.py
+++ espeonbot.py
@@ -1,5 +1,25 @@
 import os
 from dotenv import load_dotenv
+import discord
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+bot.remove_command("help")
+
+
+@bot.command()
+async def help(ctx):
+    await ctx.send("Hello Madelaine!")
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot running as {bot.user}")
+
+
+bot.run(TOKEN)
