84056e0c52421127d108e60e0e2f493dab400961
Madelaine Dalangin <madelainedalangin@gmail.com>
2026-04-27 20:32:51 -0600

    renamed to logging_tasks.py

--- cogs/logging.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands, tasks
-from db import db
-from helpers import parse_duration
-from datetime import datetime, timedelta
-
-class Logging(commands.cog):
-  
-  def __init__(self, bot):
-    self.bot = bot
